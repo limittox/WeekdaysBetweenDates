69ae51fd4dde0ad293e5111768351f0b3550551e
Yathu Arulnanthy <yathu@outlook.com>
2020-12-03 15:06:30 +1100

    Removed unnecessary files

--- WeekdaysBetweenDates/Program.cs
+++ WeekdaysBetweenDates/Program.cs
@@ -4,11 +4,6 @@ using System.Collections.Generic;
 namespace WeekdaysBetweenDates
 {
 
-    //https://stackoverflow.com/questions/4600034/calculate-number-of-weekdays-between-two-dates-in-java
-    //https://github.com/nodatime/nodatime/issues/6
-    //https://gist.github.com/timgaunt/b77e7d34c0a7f05817fa5c132bf1009a
-    //https://www.xspdf.com/resolution/56669228.html
-    // https://www.ryadel.com/en/datetime-add-subtract-calculate-get-business-days-asp-net-c-sharp-helper-class-function/ ****
     public class Program
     {
         static void Main(string[] args)
@@ -24,7 +19,7 @@ namespace WeekdaysBetweenDates
             Console.WriteLine(calc.calcWeekDaysLoop(new DateTime(1, 8, 13), new DateTime(9999, 8, 21)));
             calc.parseHolidaysSameDay("sameDayHolidays.txt");
 
-
+            
         }
     }
 }
